e5d863c0ade3652e582f9b0c88cce325b23f2fef
Niko-transfem <shuheng.huang.2201@stu.moonshotacademy.cn>
2022-09-25 12:39:08 +0800

    fix 3.1.8

--- U1_L3/3.1.8.py
+++ U1_L3/3.1.8.py
@@ -3,8 +3,9 @@ sumchi=0 # chicken
 sumcou=0 # couple
 while num <= 100:
     if num%2==0:
-        sumcou+=1
+        sumcou+=num
     else:
-        sumchi+=1
+        sumchi+=num
+#读题失败
     num +=1
 print("偶：",sumcou,"鸡",sumchi)
