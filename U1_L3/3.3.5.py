c0e784153e6892ae602d67cb1de1f1750dc16a20
Niko-transfem <shuheng.huang.2201@stu.moonshotacademy.cn>
2022-09-26 17:02:58 +0800

    3.3 fix

--- U1_L3/3.3.5.py
+++ U1_L3/3.3.5.py
@@ -1,11 +1,8 @@
 for i in range (1,5):
     for j in range (1,5):
         for k in range (1,5):
-            for l in range (1,5):
-                if i == j or i == k or i == l:
-                    continue
-                if j == k or j == l:
-                    continue
-                if k == l:
-                    continue
-                print(i,j,k,l)
+            if i == j or i == k:
+                continue
+            if j == k :
+                continue
+            print(i,j,k)
