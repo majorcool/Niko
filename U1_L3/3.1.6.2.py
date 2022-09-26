fbdc6727118b79fa4e6271a0ca0a2dfb30f6dbce
Niko-transfem <shuheng.huang.2201@stu.moonshotacademy.cn>
2022-09-26 16:33:03 +0800

    3.1.6.2

---
+++ U1_L3/3.1.6.2.py
@@ -0,0 +1,10 @@
+totscr=0
+for i in range(30):
+    print("finished stu=",i)
+    tmpscr = int (input("scr?"))
+    while ( tmpscr > 100 or tmpscr < 0):
+        print("error!")
+        tmpscr = int(input("scr?"))
+    else:
+        totscr += tmpscr
+print("finished! avg=",totscr/30)
