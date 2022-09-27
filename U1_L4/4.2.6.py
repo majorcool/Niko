bccfcfdb2098046500dd3f8c4a50a5052abaa5b9
Niko-transfem <shuheng.huang.2201@stu.moonshotacademy.cn>
2022-09-27 19:08:51 +0800

    L4 hw fix

--- U1_L4/4.2.6.py
+++ U1_L4/4.2.6.py
@@ -1,6 +1,6 @@
 def prnt(x):
     for i in range(0, x):
         for num in range(1, i + 2):
-            print(num, "*", i + 1, "=", (i + 1) * num, end='  ')
+            print(num, "*", i + 1, "=", (i + 1) * num, end='\t')
         print("")
 prnt(int(input()))
