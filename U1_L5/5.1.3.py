cd196180493e972433989e3d660fe8783d190542
Niko-transfem <shuheng.huang.2201@stu.moonshotacademy.cn>
2022-09-29 10:55:13 +0800

    5.1.3 update

--- U1_L5/5.1.3.py
+++ U1_L5/5.1.3.py
@@ -1,9 +1,11 @@
+
 list_num=[1, 3, 4, 3, 7, 3, 9, 8, 6, 3]
-while (list_num.count(3)!=0):
-    for i in range(len(list_num)):
-        if list_num[i] == 3:
-            print(i)
-            del list_num[i]
-            break
+del_index=[]
+for i in range(len(list_num)):
+    if list_num[i] == 3:
+        print(i, end = ' ')
+        del_index.append(i)
+for i in range(len(del_index)-1, -1, -1):
+    list_num.pop(del_index[i])
 
 print(list_num)
