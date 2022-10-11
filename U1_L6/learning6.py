862b779aab2b66f38d9fe8ce004524f0ed33edc4
Niko-transfem <shuheng.huang.2201@stu.moonshotacademy.cn>
2022-10-11 08:02:03 +0800

    learning6

---
+++ U1_L6/learning6.py
@@ -0,0 +1,16 @@
+code_str = "Talk is cheap. Show me the code."
+print(code_str)
+
+code_str_ = "\"Talk is cheap. Show me the code\""
+print(code_str_)
+
+poem_ = '''
+1234567890
+'''
+# len(open_) = 12
+
+picasso = "巴勃罗·迭戈·何塞·弗朗西斯科·狄·保拉·胡安·纳波穆西诺·玛莉亚"\
+    "·洛斯·雷梅迪奥斯·西普里亚诺·狄·拉·圣地西玛·特里尼达·路易斯·毕加索"
+print(picasso)
+
+print(picasso[-3:-1]+picasso[-1])
