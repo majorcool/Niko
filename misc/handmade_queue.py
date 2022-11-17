1102ee927c46a42f86c2a6f54eff0fbe211be8f5
Niko-transfem <shuheng.huang.2201@stu.moonshotacademy.cn>
2022-11-17 14:50:53 +0800

    handmade stack and queue

--- misc/handmade_queue.py
+++ misc/handmade_queue.py
@@ -1,12 +1,22 @@
 class Queue:
     def __init__(self):
         self.q = []
+
     def enqueue(self, element):
         self.q.append(element)
+
     def dequeue(self):
         return self.q.pop(0)
+
     def access(self):
         return self.q[0]
+
+    def isempty(self):
+        return not len(self.q)
+
+    def size(self):
+        return len(self)
+
 
 qq = Queue()
 qq.enqueue("first")
