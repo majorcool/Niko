807f1e275be73d777c9ef307c96a995a73fdaad4
Niko-transfem <shuheng.huang.2201@stu.moonshotacademy.cn>
2022-12-05 17:08:10 +0800

    exam10 v2

--- Exam/Exam10/2.3.py
+++ Exam/Exam10/2.3.py
@@ -1,18 +1,28 @@
 def can_place_flowers(pots, n):
+    if pots[0] == 0:
+        pots = [0] + pots
+    if pots[len(pots) - 1] == 0:
+        pots = pots + [0]
     zero_sections = []
     temp_start = 0
     temp_len = 0
-    for i in range(1,len(pots)):
+    for i in range(0,len(pots)):
         if pots[i] == 1:
             temp_start = i
-            zero_sections.append(temp_len)
+            if temp_len != 0:
+                zero_sections.append(temp_len)
             temp_len = 0
         if pots[i] == 0:
             temp_len += 1
-    zero_sections.append(temp_len)
+    if temp_len != 0:
+        zero_sections.append(temp_len)
     max_flower = 0
     for i in zero_sections:
+        if i == 0:
+            continue
+        print((i - 1) // 2)
         max_flower += (i - 1) // 2
     print(max_flower)
+    print(zero_sections)
     return n <= max_flower
-print(can_place_flowers([1,0,0,0,1],1))
+print(can_place_flowers([0,0,1,0,0],2))
